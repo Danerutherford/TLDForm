752b44c6fd50a529e8f944eed30ad1d37934340a
Dane <danerutherford94@gmail.com>
2018-02-25 11:44:28 -0500

    Fixed!

---
+++ TLDproject/TLDform/TLDForm/TLDForm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TLDForm.Startup))]
+namespace TLDForm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
