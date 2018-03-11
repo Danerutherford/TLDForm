313e620fc2ecaecb895eb17360e6b518949274b7
Dane <danerutherford94@gmail.com>
2018-03-11 11:37:18 -0400

    Submit is working
    Just need to wrap it all in bootstrap and start fixing up the Order History Page

---
+++ Capstone/Capstone/Controllers/OrdersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Capstone.Models;
+
+namespace Capstone.Controllers
+{
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private OrderDBContext db = new OrderDBContext();
+
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View(db.Orders.ToList());
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderModel orderModel = db.Orders.Find(id);
+            if (orderModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderModel);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "orderId,AccountNbr,AccountName1,AccountName2,Fname,Lname,IDNbr,HolderType,Neutron,WLocation,UPD,Sname,ClipType,SeriesColor,FreqColor,BadgeUse")] OrderModel orderModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(orderModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(orderModel);
+        }
+
+        // GET: Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderModel orderModel = db.Orders.Find(id);
+            if (orderModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderModel);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "orderId,AccountNbr,AccountName1,AccountName2,Fname,Lname,IDNbr,HolderType,Neutron,WLocation,UPD,Sname,ClipType,SeriesColor,FreqColor,BadgeUse")] OrderModel orderModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(orderModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(orderModel);
+        }
+
+        // GET: Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderModel orderModel = db.Orders.Find(id);
+            if (orderModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderModel);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            OrderModel orderModel = db.Orders.Find(id);
+            db.Orders.Remove(orderModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
