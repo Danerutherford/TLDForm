b710a9c92320b006c60eb38b978174cf1f14ad3e
Aaron Chestnut <aaronchestnut@gmail.com>
2018-03-13 03:42:17 -0400

    Authorize user works now. Have to fix some security issues, and adjust link registration.

--- Capstone/Capstone/Controllers/HomeController.cs
+++ Capstone/Capstone/Controllers/HomeController.cs
@@ -50,21 +50,21 @@ namespace Capstone.Controllers
             {
                 var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
                 var message = new MailMessage();
-                message.To.Add(new MailAddress("recipient@gmail.com"));  // replace with valid value 
-                message.From = new MailAddress("sender@outlook.com");  // replace with valid value
+                message.To.Add(new MailAddress(model.FromEmail));  // replace with valid value 
+                message.From = new MailAddress("testcapstonetest@gmail.com");  // replace with valid value
                 message.Subject = "Your email subject";
-                message.Body = string.Format(body, "TLD FORM", model.FromEmail, model.Message);
+                message.Body = string.Format(body, "TLD FORM", "Testing", model.Message);
                 message.IsBodyHtml = true;
 
                 using (var smtp = new SmtpClient())
                 {
                     var credential = new NetworkCredential
                     {
-                        UserName = "user@outlook.com",  // replace with valid value
-                        Password = "password"  // replace with valid value
+                        UserName = "testcapstonetest@gmail.com",  // replace with valid value
+                        Password = "qeadzc1!"  // replace with valid value
                     };
                     smtp.Credentials = credential;
-                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Host = "smtp.gmail.com";
                     smtp.Port = 587;
                     smtp.EnableSsl = true;
                     await smtp.SendMailAsync(message);
