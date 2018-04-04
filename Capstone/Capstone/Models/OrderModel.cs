92218f2a741f3077f3e3af3aac0df6ddc92ec750
Dane <danerutherford94@gmail.com>
2018-04-04 16:29:12 -0400

    Updated the data model to be more comprehensive

--- Capstone/Capstone/Models/OrderModel.cs
+++ Capstone/Capstone/Models/OrderModel.cs
@@ -19,18 +19,25 @@ namespace Capstone.Models
         [Display(Name = "Number of Badges")]
         public int NumBadge { get; set; }
 
+        [StringLength(6, ErrorMessage = "Cannot be longer than 6 characters.")]
         public int AccountNbr { get; set; }
 
+        [StringLength(17, ErrorMessage = "Cannot be longer than 17 characters.")]
         public string AccountName1 { get; set; }
 
+        [StringLength(17, ErrorMessage = "Cannot be longer than 17 characters.")]
         public string AccountName2 { get; set; }
 
+        [StringLength(17, ErrorMessage = "Cannot be longer than 17 characters.")]
         public string Fname { get; set; }
 
+        [StringLength(17, ErrorMessage = "Cannot be longer than 17 characters.")]
         public string Lname { get; set; }
 
+        [StringLength(8, ErrorMessage = "Cannot be longer than 8 characters.")]
         public int IDNbr { get; set; }
 
+        [StringLength(2, ErrorMessage = "Cannot be longer than 2 characters.")]
         string HType = "LA";
         public string HolderType {
             get
@@ -43,28 +50,34 @@ namespace Capstone.Models
                 HType = value;
             }
         }
-           
 
 
 
-    public string Neutron { get; set; }
+        [StringLength(2, ErrorMessage = "Cannot be longer than 2 characters.")]
+        public string Neutron { get; set; }
 
         public char BarCode { get; set; }
 
         public string WLocation { get; set; }
 
+        [Required(ErrorMessage = "This is a required field")]
         public string UPD { get; set; }
 
+        [Required(ErrorMessage = "This is a required field")]
         public string Sname { get; set; }
 
         public char CustomerKey { get; set; }
 
+        [Required(ErrorMessage = "This is a required field")]
         public string ClipType { get; set; }
 
+        [Required(ErrorMessage = "This is a required field")]
         public string SeriesColor { get; set; }
 
+        [Required(ErrorMessage = "This is a required field")]
         public string FreqColor { get; set; }
 
+        [Required(ErrorMessage = "This is a required field")]
         public string BadgeUse { get; set; }
 
       
