da9e08a88a0303cfacf3852fa0f48fa90cc2c428
Dane <danerutherford94@gmail.com>
2018-04-15 17:35:47 -0400

    Doing a little work in the db

--- Capstone/Capstone/Models/AccountViewModels.cs
+++ Capstone/Capstone/Models/AccountViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 
 namespace Capstone.Models
 {
@@ -108,5 +109,16 @@ namespace Capstone.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+    }
+
+    public class AccountViewModelsContext : DbContext
+    {
+        public AccountViewModelsContext() : base("Capstone")
+        {
+            Database.SetInitializer<AccountViewModelsContext>(new DropCreateDatabaseIfModelChanges<AccountViewModelsContext>());
+        }
+
+
+
     }
 }
