e5c8d49cf764e711665af68247286a1fbb8fc502
Dane <danerutherford94@gmail.com>
2018-04-24 17:45:48 -0400

    Reworking ddl's to work with adding and deleting stuff from them

---
+++ Capstone/Capstone/Controllers/DropDownListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone.Controllers
+{
+    public class DropDownListController : Controller
+    {
+        // GET: DropDownList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
